f5eb50a1aab6abbb9199684649d4757343728ca2
RooT_Kim <bdd3452@gmail.com>
2022-06-09 18:45:50 +0900

    Section component

---
+++ react_practice/src/components/section.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import List from './list';
+import Write from './write';
+
+const Section = () => {
+  return (
+    <>
+      <List name='Root' />
+    </>
+  );
+};
+
+export default Section;
