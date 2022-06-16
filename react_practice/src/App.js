fa409e8286e31ff9b007a7a348283f177dad3364
RooT_Kim <bdd3452@gmail.com>
2022-06-16 23:20:27 +0900

    Header and Footer

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -2,10 +2,28 @@
 // import './App.css';
 import Section from './components/section';
 
+function Header() {
+  return (
+    <header>
+      <h1>Hello</h1>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <h6>Bye</h6>
+    </footer>
+  );
+}
+
 function App() {
   return (
     <div className='App'>
+      <Header></Header>
       <Section />
+      <Footer></Footer>
     </div>
   );
 }
