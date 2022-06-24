0c6c3c89a3a3da6994f7a7aeeee8ba5a0a888b7a
RooT_Kim <bdd3452@gmail.com>
2022-06-24 16:17:28 +0900

    React Hook deep study start

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,8 +1,12 @@
+import { useState, useEffect, useContext, useReducer, useInsertionEffect } from 'react';
+
 const App = () => {
+  console.log(useState('Foori'));
+  const [name, setName] = useState('Foori');
   return (
     <div>
       <p>안녕하세요, 저는</p>
-      <Name checked='true' name='Foori' />
+      <Name checked='true' name={name} />
       <p>입니다</p>
     </div>
   );
