3334b6590468a01149e8c81250f3bdb7c08ef434
RooT_Kim <bdd3452@gmail.com>
2022-07-02 19:47:32 +0900

    Testing...

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -3,10 +3,14 @@ import { useState, useEffect, useContext, useReducer, useInsertionEffect } from 
 const App = () => {
   console.log(useState('Foori'));
   const [name, setName] = useState('Foori');
+  const nameHandler = () => {
+    setName((c) => 'HI' + Math.random() * 1000);
+  };
   return (
     <div>
       <p>안녕하세요, 저는</p>
       <Name checked='true' name={name} />
+      <input type='button' onClick={nameHandler}></input>
       <p>입니다</p>
     </div>
   );
